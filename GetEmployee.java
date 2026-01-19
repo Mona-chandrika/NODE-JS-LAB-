68aeac53199b11af7bed7a27d578d5f8d5cfe511
Mona-chandrika <24WH1A0570@bvrithyderabad.edu.in>
2026-01-19 11:06:49 +0530

    Add files via upload

---
+++ GetEmployee.java
@@ -0,0 +1,32 @@
+package proj;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GetEmployee{
+	public static void main(String[] args) {
+		try {
+			 //step1 : register the Driver
+			//DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			
+			//Step2: Connect the server by supplying URL(ip,port,uname,pwd)
+			Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/24wh1a0570","root","1234");
+			
+			//step3: create Statement object to send queries to DB
+			Statement statement = connection.createStatement();
+			
+			//step4 : execute query
+			ResultSet rs=statement.executeQuery("select * from emp");
+			while(rs.next()) {
+				System.out.println(rs.getString("EMPNO") +"\t"+rs.getString("ENAME")+"\t"+rs.getString("JOB"));
+			}
+			//step5: close the coonection
+			connection.close(); 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
