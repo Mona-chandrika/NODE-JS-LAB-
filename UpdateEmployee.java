630a8b25b82d89c9d9245c16487930f0cc8e6d32
Mona-chandrika <24WH1A0570@bvrithyderabad.edu.in>
2026-02-02 09:36:08 +0530

    UpdateEmployee.java

--- UpdateEmployee.java
+++ UpdateEmployee.java
@@ -1,34 +1,42 @@
 package proj;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class GetEmployee{
-	public static void main(String[] args) {
-		try {
-			 //step1 : register the Driver
-			//DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			
-			//Step2: Connect the server by supplying URL(ip,port,uname,pwd)
-			Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/24wh1a0570","root","1234");
-			
-			//step3: create Statement object to send queries to DB
-			Statement statement = connection.createStatement();
-			
-			//step4 : execute query
-			//ResultSet rs=statement.executeUpdate("select * from emp");
-			// executeUpdate for non select commands like update and delete.
-			while(rs.next()) {
-				System.out.println(rs.getString("EMPNO") +"\t"+rs.getString("ENAME")+"\t"+rs.getString("JOB"));
-			}
-			//step5: close the coonection
-			connection.close(); 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-}
+public class UpdateEmployee {
 
+    public static void main(String[] args) {
+
+        try {
+            // Step 1: Connect to DB
+            Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/24wh1a0570?useSSL=false&allowPublicKeyRetrieval=true",
+                "root",
+                "1234"
+            );
+
+            // Step 2: Create Statement
+            Statement statement = connection.createStatement();
+
+            // Step 3: Execute UPDATE query
+            String sql = "UPDATE emp SET JOB='MANAGER' WHERE EMPNO=1";
+
+            int rows = statement.executeUpdate(sql);
+
+            // Step 4: Check result
+            if (rows > 0) {
+                System.out.println("Employee updated successfully ");
+            } else {
+                System.out.println("No employee found with given EMPNO");
+            }
+
+            // Step 5: Close connection
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
